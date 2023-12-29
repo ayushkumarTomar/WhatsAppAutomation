9ab598257ca54f4536aa8d742eec62e289c586e3
Sankritya14 <sankrityashukla@gmail.com>
2023-12-29 22:03:00 +0530

    index.js and phoneController

---
+++ src/components/index.jsx
@@ -0,0 +1,23 @@
+import ChatGPTComponent from "./ChatGptComponent";
+import ConfigMsg from "./ConfigMsg";
+import Queues from "./Queues";
+import SettingsPanel from "./SettingsPanel";
+import Upload from "./Upload";
+import Groups from "./Groups";
+import Editor from "./Editor";
+import PhoneControl from "./PhoneControl";
+import AutoPrompt from "./AutoPrompt";
+
+
+export {ChatGPTComponent , 
+        ConfigMsg ,
+        Queues ,
+        SettingsPanel , 
+        Upload , 
+        Groups ,
+        Editor , 
+        PhoneControl , 
+        AutoPrompt
+    
+    
+    };
